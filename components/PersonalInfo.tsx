1bc69ad696d7e6a9441c936ebabc729de3095d10
Aditya Goyal <adityagoyaldoon@gmail.com>
2025-03-06 23:44:20 -0500

    Added image editing option

--- components/PersonalInfo.tsx
+++ components/PersonalInfo.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native" 
-import { Input, Button } from "@rneui/themed"
+import React, { useState } from "react";
+import { View, Text, Image, Button } from "react-native" 
+import { Input } from "@rneui/themed"
+import { launchImageLibrary } from "react-native-image-picker";
 
 
 export const PersonalInfo = () => {
+    const [imageUri, setImageUri] = useState<string | null>(null);
+
+    const handleChoosePhoto = () => {
+        // launchImageLibrary({ mediaType: 'photo' }, (response) => {
+        //     if (response.assets && response.assets.length > 0) {
+        //         setImageUri(response.assets[0].uri);
+        //     }
+        // });
+        console.log("Trying to add image")
+    };
 
     return (
         <View className="flex-1 bg-red-100 mx-2 gap-2">
             <Text className="text-3xl">Edit Personal Information</Text>
+
+            {imageUri && (
+                <Image
+                    source={{ uri: imageUri }}
+                    style={{ width: 100, height: 100, borderRadius: 50 }}
+                />
+            )}
+            <Button title="Upload Image" onPress={handleChoosePhoto} />
 
             <Input 
                 placeholder="Your Name"
@@ -21,6 +41,10 @@ export const PersonalInfo = () => {
                 placeholder="Body Weight"
             />
             <Button title="Save Weight" />
+            <Input
+                placeholder="Measurement Unit"
+            />
+            <Button title="Save Unit" />
         </View>
     )
 }
